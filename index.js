61abcca1510a3549d22aaac793a467ace57d296d
nazhot <15216154+nazhot@users.noreply.github.com>
2023-02-27 20:30:53 -0700

    Wagering Setup on Server
    Added a lot of the code required for wagering on the server side. This
    included checking to make sure the wager is valid (number, turned to
    int, and is less than player's current tokens), figuring out the winner,
    adding the card/lowest wager combo to that players deck, etc.

--- index.js
+++ index.js
@@ -16,6 +16,59 @@ app.get("/", (req, res) => {
 const data = {
     numPlayers: 0,
     players: {},
+    currentCard: "Test",
+    currentWagers: {},
+    cardList: [],
+    addCardToPlayer(player){
+        this.players[player].cards.push({
+            card:  this.currentCard,
+            value: this.getLowestWager(),
+        })
+    },
+    resetWagered(){
+        for(const player in this.players){
+            this.players[player].wagered = false;
+        }
+    },
+    getPlayersWithHighestWager() {
+        let highestWager   = -1;
+        let highestPlayers = [];
+        for(const [player, wager] of Object.entries(this.currentWagers)){
+            if (wager === highestWager){
+                highestPlayers.push(player);
+                continue;
+            }
+            if (wager > highestWager){
+                highestWager   = wager;
+                highestPlayers = [player];
+            }
+        }
+        return highestPlayers;
+    },
+    getLowestWager() {
+        let lowestWager = Infinity;
+        for (const player in this.currentWagers){
+            const wager = this.currentWagers[player];
+            lowestWager = Math.min(lowestWager, wager);
+        }
+        return lowestWager;
+    },
+    getPlayersWithLeastTieWins() {
+        let lowestTieWins = 100;
+        let lowestPlayers = [];
+        for(const player in this.players){
+            const ties = this.players[player].tieWins;
+            if (ties === lowestTieWins){
+                lowestPlayers.push(player);
+                continue;
+            }
+            if (ties < lowestTieWins){
+                lowestTieWins = ties;
+                lowestPlayers = [player];
+            }
+        }
+        return lowestPlayers;
+    }
 };
 
 function defaultPlayerData(){
@@ -25,12 +78,13 @@ function defaultPlayerData(){
         cards: [], //format should be {card number, price you paid, price opponent paid}
         wagered: false,
         liquidated: false,
+        tieWins: 0,
     };
 }
 
 function checkAllProperties(propertyName){
     for (const prop in data.players){
-        if (!prop[propertyName]){
+        if (!data.players[prop][propertyName]){
             return false;
         }
     }
@@ -67,6 +121,15 @@ function emitChangeToLiquidateScreen(player){
     io.to(player).emit("change-to-liquidate-screen");
 }
 
+function emitNonNumberWager(player){
+    io.to(player).emit("non-number-wager");
+}
+
+function isNumeric(stringToCheck){
+    if (typeof stringToCheck !== "string") return false;
+    return !isNaN(stringToCheck) &&
+           !isNaN(parseFloat(stringToCheck));
+}
 
 
 io.on("connection", (socket) => {
@@ -91,17 +154,45 @@ io.on("connection", (socket) => {
     socket.on("place-wager", (tokensWagered) => {
         const playerData = data.players[socket.id];
 
+        if (!isNumeric(tokensWagered)){
+            emitNonNumberWager(socket.id);
+            return;
+        }
+
+        tokensWagered = Math.max(parseInt(tokensWagered), 0);
+
         if (playerData.tokens < tokensWagered) {
             emitInsufficientTokens(socket.id);
             return;
         }
 
-        playerData.tokens -= tokensWagered;
-        
-        playerData.wagered = true;
+        playerData.tokens            -= tokensWagered;
+        data.currentWagers[socket.id] = tokensWagered;
+        playerData.wagered            = true;
+
         const allWagered = checkIfAllPlayersWagered();
-        console.log(data);
+        //console.log(data);
         console.log(allWagered);
+        if (allWagered){
+            const winningPlayers    = data.getPlayersWithHighestWager();
+            const numWinningPlayers = winningPlayers.length;
+            const lowestWager       = data.getLowestWager();
+            let   winningPlayer     = null;
+            if (numWinningPlayers > 1){
+                const elligiblePlayers = data.getPlayersWithLeastTieWins();
+                      winningPlayer    = elligiblePlayers[Math.floor(Math.random()*elligiblePlayers.length)];
+                      data.players[winningPlayer].tieWins++;
+            } else {
+                winningPlayer = winningPlayers[0];
+            }
+
+            data.addCardToPlayer(winningPlayer);
+            data.currentWagers = {};
+            data.resetWagered();
+            console.log(data);
+
+        }
+
         emitWagerPlaced(socket.id, playerData.name, playerData.tokens);
         
     });
