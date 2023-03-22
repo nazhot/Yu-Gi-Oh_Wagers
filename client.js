ee36cddb6f30134efad89fa18b2f96d3c3bd5934
nazhot <15216154+nazhot@users.noreply.github.com>
2023-03-22 10:27:51 -0600

    Allow Wagered Description Scroll
    Closes #32
    The wagered cards description now is able to be scrolled if it overflows
    the left column. Also lowered the size of the description text so most
    fit anyways. Hovered card section now also allows the overflow, but you
    can't really scroll the whole section because you are hovering over a
    card.

--- client.js
+++ client.js
@@ -6,194 +6,200 @@ const smallCardImageUrl = "https://images.ygoprodeck.com/images/cards_small/";
 const largeCardImageUrl = "https://images.ygoprodeck.com/images/cards/";
 
 //allow player to press enter after typing in their wager amount
-document.getElementById("player-wager-amount").addEventListener("keypress", (event) => {
-if (event.key === "Enter"){
-    event.preventDefault();
-    document.getElementById("submit-wager").click();
-}
-});
+document
+  .getElementById("player-wager-amount")
+  .addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      document.getElementById("submit-wager").click();
+    }
+  });
 
-function toggleCardSelect(cardElement){
-if (cardElement.classList.contains("selected-card")) {
+function toggleCardSelect(cardElement) {
+    if (cardElement.classList.contains("selected-card")) {
     cardElement.classList.remove("selected-card");
-} else {
+    } else {
     cardElement.classList.add("selected-card");
-}
-}
-
-function submitReadyStatus(status){
-socket.emit("update-readied-status", status);
-}
-
-function submitRequestEnd(element){
-socket.emit("request-end");
-element.style.opacity = "0.3";
-}
-
-function submitDownload(){
-socket.emit("download");
-}
-
-function swapReadyStatus(readyButtonElement){
-const elementClass = readyButtonElement.className;
-if (elementClass === "ready"){
-    readyButtonElement.className = "not-ready";
-    readyButtonElement.innerHTML = "NOT READY";
-} else if (elementClass === "not-ready"){
-    readyButtonElement.className = "ready";
-    readyButtonElement.innerHTML = "READY";
-}
-
-submitReadyStatus(readyButtonElement.className === "ready");
-}
-
-function submitWager(){
-const wagerElement = document.getElementById("player-wager-amount");
-const wager        = wagerElement.value;
-socket.emit("place-wager", wager);
-}
-
-function resetDeckScroll(){
-const deckElements = document.getElementsByClassName("deck-container");
-for (const deckElement of deckElements){
-    deckElement.scrollTop = 0;
-}
-}
-
-function submitLiquidate(){
-const liquidateElements = document.getElementsByClassName("selected-card");
-const cardIdsToLiquidate = [];
-for (const cardToLiquidate of liquidateElements){
-    cardIdsToLiquidate.push(cardToLiquidate.cardId);
-}
-socket.emit("liquidate-cards", cardIdsToLiquidate);
-}
-
-function hideAllElementsByClassName(className){
-const elements = document.getElementsByClassName(className);
-for (const element of elements){
-    element.classList.remove("hidden");
-    element.classList.add("hidden");
-}
-}
-
-function showAllElementsByClassName(className){
-const elements = document.getElementsByClassName(className);
-for (const element of elements){
-    element.classList.remove("hidden");
-}
-}
-
-function resetEndButtonOpacity(){
-const endButtons = document.getElementsByClassName("request-end");
-for (const endButton of endButtons){
-    endButton.style.opacity = "1.0";
-}
-}
-
-function changeToScreen(screenName){
-const screen     = document.getElementById(screenName);
-
-hideAllElementsByClassName("screen-container");
-resetEndButtonOpacity();
-
-screen.classList.remove("hidden");
-resetDeckScroll();
-
-return screen;
-}
-
-function setTokenCount(tokens){
-setInfoPanes("player-token-count", tokens);
-}
-
-function setDeckSize(deckSize){
-setInfoPanes("player-deck-size", deckSize);
-}
-
-function setRemainingCards(cardsRemaining){
-setInfoPanes("player-cards-remaining", cardsRemaining);
-}
-
-function setInfoPanes(elementClass, value){
-const elements = document.getElementsByClassName(elementClass);
-for (const element of elements){
-    element.innerHTML = value;
-}
-}
-
-function clearDeck(){
-const deckElements = document.getElementsByClassName("deck");
-for (const deckElement of deckElements){
-    while (deckElement.firstChild){
-    deckElement.removeChild(deckElement.lastChild);
     }
 }
-return deckElements;
+
+function submitReadyStatus(status) {
+    socket.emit("update-readied-status", status);
 }
 
-function setWagerCard(cardObject){
-const shownCard = document.getElementById("shown-card");
-shownCard.src = largeCardImageUrl + cardObject.id + ".jpg";
-document.getElementById("wagered-card-name").innerHTML = cardObject.name;
-document.getElementById("wagered-card-desc").innerHTML = cardObject.desc;
+function submitRequestEnd(element) {
+    socket.emit("request-end");
+    element.style.opacity = "0.3";
+}
+
+function submitDownload() {
+    socket.emit("download");
+}
+
+function swapReadyStatus(readyButtonElement) {
+    const elementClass = readyButtonElement.className;
+    if (elementClass === "ready") {
+    readyButtonElement.className = "not-ready";
+    readyButtonElement.innerHTML = "NOT READY";
+    } else if (elementClass === "not-ready") {
+    readyButtonElement.className = "ready";
+    readyButtonElement.innerHTML = "READY";
+    }
+
+    submitReadyStatus(readyButtonElement.className === "ready");
+}
+
+function submitWager() {
+    const wagerElement = document.getElementById("player-wager-amount");
+    const wager = wagerElement.value;
+    socket.emit("place-wager", wager);
+}
+
+function resetDeckScroll() {
+    const deckElements = document.getElementsByClassName("deck-container");
+    for (const deckElement of deckElements) {
+    deckElement.scrollTop = 0;
+    }
+}
+
+function submitLiquidate() {
+    const liquidateElements = document.getElementsByClassName("selected-card");
+    const cardIdsToLiquidate = [];
+    for (const cardToLiquidate of liquidateElements) {
+    cardIdsToLiquidate.push(cardToLiquidate.cardId);
+    }
+    socket.emit("liquidate-cards", cardIdsToLiquidate);
+}
+
+function hideAllElementsByClassName(className) {
+    const elements = document.getElementsByClassName(className);
+    for (const element of elements) {
+    element.classList.remove("hidden");
+    element.classList.add("hidden");
+    }
+}
+
+function showAllElementsByClassName(className) {
+    const elements = document.getElementsByClassName(className);
+    for (const element of elements) {
+    element.classList.remove("hidden");
+    }
+}
+
+function resetEndButtonOpacity() {
+    const endButtons = document.getElementsByClassName("request-end");
+    for (const endButton of endButtons) {
+    endButton.style.opacity = "1.0";
+    }
+}
+
+function changeToScreen(screenName) {
+    const screen = document.getElementById(screenName);
+
+    hideAllElementsByClassName("screen-container");
+    resetEndButtonOpacity();
+
+    screen.classList.remove("hidden");
+    resetDeckScroll();
+
+    return screen;
+}
+
+function setTokenCount(tokens) {
+    setInfoPanes("player-token-count", tokens);
+}
+
+function setDeckSize(deckSize) {
+    setInfoPanes("player-deck-size", deckSize);
+}
+
+function setRemainingCards(cardsRemaining) {
+    setInfoPanes("player-cards-remaining", cardsRemaining);
+}
+
+function setInfoPanes(elementClass, value) {
+    const elements = document.getElementsByClassName(elementClass);
+    for (const element of elements) {
+    element.innerHTML = value;
+    }
+}
+
+function clearDeck() {
+    const deckElements = document.getElementsByClassName("deck");
+    for (const deckElement of deckElements) {
+    while (deckElement.firstChild) {
+        deckElement.removeChild(deckElement.lastChild);
+    }
+    }
+    return deckElements;
+}
+
+function setWagerCard(cardObject) {
+    const shownCard = document.getElementById("shown-card");
+    shownCard.src = largeCardImageUrl + cardObject.id + ".jpg";
+    document.getElementById("wagered-card-name").innerHTML = cardObject.name;
+    document.getElementById("wagered-card-desc").innerHTML = cardObject.desc;
 }
 
 function download(filename, text) {
-const element = document.createElement('a');
-        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-        element.setAttribute('download', filename);
-        element.style.display = 'none';
+    const element = document.createElement("a");
+    element.setAttribute(
+    "href",
+    "data:text/plain;charset=utf-8," + encodeURIComponent(text)
+    );
+    element.setAttribute("download", filename);
+    element.style.display = "none";
 
-document.body.appendChild(element);
+    document.body.appendChild(element);
 
-element.click();
+    element.click();
 
-document.body.removeChild(element);
+    document.body.removeChild(element);
 }
 
 /**
  * Create the standard cardElement that's used throughout the program
  * @param {string} cardId id of the card
  */
-function generateCardElement(cardObject){
-const cardId = cardObject.id;
-const cardWager = cardObject.wager;
-const card = document.createElement("img");
-card.classList.add("deck-card")
-card.src = smallCardImageUrl + cardId + ".jpg";
-card.cardId = cardId;
-card.wager  = cardWager;
-card.cardName = cardObject.name;
-card.cardDesc = cardObject.desc;
-card.largeImage = largeCardImageUrl + cardId + ".jpg";
-card.onmouseover = () => {
-    const largeCardElements     = document.getElementsByClassName("large-card");
-    const previousWagerElements = document.getElementsByClassName("previous-wager");
+function generateCardElement(cardObject) {
+    const cardId            = cardObject.id;
+    const cardWager         = cardObject.wager;
+    const card              = document.createElement("img");
+    const largeCardElements = document.getElementsByClassName("large-card");
 
-    for (const largeCardElement of largeCardElements){
-    largeCardElement.src = card.largeImage;
+    card.classList.add("deck-card");
+    card.src = smallCardImageUrl + cardId + ".jpg";
+    card.cardId = cardId;
+    card.wager = cardWager;
+    card.cardName = cardObject.name;
+    card.cardDesc = cardObject.desc;
+    card.largeImage = largeCardImageUrl + cardId + ".jpg";
+    card.onmouseover = () => {
+
+    for (const largeCardElement of largeCardElements) {
+        largeCardElement.src = card.largeImage;
     }
+
     setInfoPanes("previous-wager", card.wager);
     setInfoPanes("hovered-card-name", card.cardName);
     setInfoPanes("hovered-card-desc", card.cardDesc);
-};
+    };
 
-card.onmouseleave = () => {
+    card.onmouseleave = () => {
     const largeCardElements = document.getElementsByClassName("large-card");
-    for (const largeCardElement of largeCardElements){
-    largeCardElement.src = cardBackImage;
+    for (const largeCardElement of largeCardElements) {
+        largeCardElement.src = cardBackImage;
     }
     setInfoPanes("previous-wager", "");
     setInfoPanes("hovered-card-name", "");
     setInfoPanes("hovered-card-desc", "");
-};
+    };
 
-card.onclick = () => {
+    card.onclick = () => {
     toggleCardSelect(card);
-}
+    };
 
-return card;
+    return card;
 }
 
 /**
@@ -204,123 +210,121 @@ return card;
  *   the game, and every time a card is added generateCardElement should be called to make it
  * @param {string} cardId id of the card to add
  */
-function addCard(cardObject){
-const deckElements = document.getElementsByClassName("deck");
+function addCard(cardObject) {
+    const deckElements = document.getElementsByClassName("deck");
 
-for (const deckElement of deckElements){
+    for (const deckElement of deckElements) {
     const cardElement = generateCardElement(cardObject); //for each deck element, a new cardElement must be generated. Learned the hard way
     deckElement.appendChild(cardElement);
-}
+    }
 }
 
 /**
  * Add all of the standard card elements to the player's deckElements from the given cardList
  * @param {array} cardList array of cardIds to be added
  */
-function setDeck(cardList){
-const deckElements = clearDeck();
-for (const cardObject of cardList){
-    for (const deckElement of deckElements){
-    const cardElement = generateCardElement(cardObject); //new card element must be generated for every new element it's being added to
-    deckElement.appendChild(cardElement);
+function setDeck(cardList) {
+    const deckElements = clearDeck();
+    for (const cardObject of cardList) {
+    for (const deckElement of deckElements) {
+        const cardElement = generateCardElement(cardObject); //new card element must be generated for every new element it's being added to
+        deckElement.appendChild(cardElement);
     }
-}   
+    }
 }
 
-function submitStartGame(){
-console.log("Starting game");
-socket.emit("start-game");
+function submitStartGame() {
+    console.log("Starting game");
+    socket.emit("start-game");
 }
 
 socket.on("non-number-wager", () => {
-console.log("Non number wager");
+    console.log("Non number wager");
 });
 
 socket.on("insufficient-tokens", () => {
-console.log("Insufficient tokens");
+    console.log("Insufficient tokens");
 });
 
 socket.on("token-update", (tokensRemaining) => {
-console.log("updating token count");
-setTokenCount(tokensRemaining);
+    console.log("updating token count");
+    setTokenCount(tokensRemaining);
 });
 
 socket.on("deck-size-update", (deckSize) => {
-setDeckSize(deckSize);
+    setDeckSize(deckSize);
 });
 
 socket.on("cards-remaining-update", (cardsRemaining) => {
-setRemainingCards(cardsRemaining);
+    setRemainingCards(cardsRemaining);
 });
 
 socket.on("self-wager-placed", (tokensRemaining) => {
-setTokenCount(tokensRemaining);
+    setTokenCount(tokensRemaining);
 });
 
 socket.on("wager-card", (cardObject) => {
-setWagerCard(cardObject);
+    setWagerCard(cardObject);
 });
 
-socket.on("wager-placed", (playerThatPlacedWager) => {
-
-});
+socket.on("wager-placed", (playerThatPlacedWager) => {});
 
 socket.on("invalid-liquidation", () => {
-console.log("Invalid liquidation");
+    console.log("Invalid liquidation");
 });
 
 socket.on("change-to-wager-screen", () => {
-changeToScreen("wagering-page");
-document.getElementById("player-wager-amount").focus();
+    changeToScreen("wagering-page");
+    document.getElementById("player-wager-amount").focus();
 });
 
 socket.on("change-to-liquidate-screen", () => {
-changeToScreen("liquidating-page");
-document.getElementById("liquidate-cards").focus();
+    changeToScreen("liquidating-page");
+    document.getElementById("liquidate-cards").focus();
 });
 
 socket.on("change-to-end-screen", () => {
-changeToScreen("end-page");
+    changeToScreen("end-page");
 });
 
 socket.on("show-end-button", () => {
-showAllElementsByClassName("request-end");
+    showAllElementsByClassName("request-end");
 });
 
 socket.on("hide-end-button", () => {
-hideAllElementsByClassName("request-end");
+    hideAllElementsByClassName("request-end");
 });
 
 socket.on("download", (ydkFile) => {
-download("WagerDeck.ydk", ydkFile);
+    download("WagerDeck.ydk", ydkFile);
 });
 
 socket.on("update-readied-statuses", (playerStatuses) => {
-let allReady = true;
-for (const player in playerStatuses){
+    let allReady = true;
+    for (const player in playerStatuses) {
     const playerReady = playerStatuses[player];
-    if (allReady && !playerReady){
-    allReady = false;
+    if (allReady && !playerReady) {
+        allReady = false;
     }
-}
+    }
 
-const startGameButton = document.getElementById("start-game-button");
+    const startGameButton = document.getElementById("start-game-button");
 
-if (allReady){
+    if (allReady) {
     startGameButton.style.opacity = "1.0";
-} else {
+    } else {
     startGameButton.style.opacity = "0.3";
-}
+    }
 });
 
 socket.on("is-player-1", () => {
-showAllElementsByClassName("player-1-element");
+    showAllElementsByClassName("player-1-element");
 });
 
 socket.on("card-added", (cardObject) => {
-addCard(cardObject);
+    addCard(cardObject);
 });
 
 socket.on("deck", (cardList) => {
-setDeck(cardList);
+    setDeck(cardList);
 });
